0c514a49b152cb15eb51487f0d902ca1d72aaaa5
Navneet Yadav <111699750+Yadav-navy@users.noreply.github.com>
2024-06-06 15:12:33 +0530

    Time: 24 ms (13.99%), Space: 21.6 MB (38.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
         int i = 0;
-        int j = i + 1;
         int size = nums.size();
+        for (int j =0; j < size; j++){
+            if (nums[j] != 0){
+                swap(nums[j],nums[i]);
+                i++;       }
+            }
         
-        while ( j < size ){
-            if( nums [i] == 0 && nums[j] != 0 ){
-                swap(nums[i],nums[j]);
-                j++;
-                i++;
-            }
-            else if (nums[i]==0 && nums[j]==0){
-                j++;
-            }
-            else if (nums[i] !=0 && nums[j]!=0){
-                i++;
-                j++;
-            }
-            else if (nums[i] !=0 && nums[j]==0){
-                i++;
-                j++;
-            }
-        }
-    }
-};
+        
+    }};
+
+        
+//         int i = 0;
+//         int j = i + 1;
+//         int size = nums.size();
+        
+//         while ( j < size ){
+//             if( nums [i] == 0 && nums[j] != 0 ){
+//                 swap(nums[i],nums[j]);
+//                 j++;
+//                 i++;
+//             }
+//             else if (nums[i]==0 && nums[j]==0){
+//                 j++;
+//             }
+//             else if (nums[i] !=0 && nums[j]!=0){
+//                 i++;
+//                 j++;
+//             }
+//             else if (nums[i] !=0 && nums[j]==0){
+//                 i++;
+//                 j++;
+//             }
+//         }
+//     }
+// };
