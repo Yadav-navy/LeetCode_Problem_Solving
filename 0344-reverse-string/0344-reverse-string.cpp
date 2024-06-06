865e9d40ceb6182b6c980d4e72c7d7e340739460
Navneet Yadav <111699750+Yadav-navy@users.noreply.github.com>
2024-06-06 12:37:04 +0530

    Time: 20 ms (36.77%), Space: 27 MB (58.93%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
+//         Now using two pointer approch
         
-        int size = s.size();
-        int j ;
-        cout << "size" << size << endl;
+        int start = 0;
+        int end = s.size()-1;
         
-        if (size % 2 == 0){         //even number of elements
-            for (int i=0 ; i< (size)/2 ; i++){
-            j = size - i - 1;
-            cout << i << " " << j << endl;
-            swap(s[i],s[j]); }    
+        while (start <= end){
+            swap(s[start],s[end]);
+            start += 1;
+            end -= 1;   
         }
-        else{
-            for (int i=0 ; i< (size-1)/2 ; i++){
-            j = size - i - 1;
-            cout << i << " " << j << endl;
-            swap(s[i],s[j]); }
-        }
-
     }
 };
+
+
+
+//         int size = s.size();
+//         int j ;
+//         cout << "size" << size << endl;
+        
+//         if (size % 2 == 0){         //even number of elements
+//             for (int i=0 ; i< (size)/2 ; i++){
+//             j = size - i - 1;
+//             cout << i << " " << j << endl;
+//             swap(s[i],s[j]); }    
+//         }
+//         else{
+//             for (int i=0 ; i< (size-1)/2 ; i++){
+//             j = size - i - 1;
+//             cout << i << " " << j << endl;
+//             swap(s[i],s[j]); }
+//         }
+
+//     }
+// };
