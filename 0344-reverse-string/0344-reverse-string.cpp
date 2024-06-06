e26aaff776e04ec3a0f9eedfb0129de61207eea1
Navneet Yadav <111699750+Yadav-navy@users.noreply.github.com>
2024-06-06 12:41:45 +0530

    Time: 21 ms (25.97%), Space: 27 MB (21.09%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -6,7 +6,7 @@ public:
         int start = 0;
         int end = s.size()-1;
         
-        while (start <= end){
+        while (start < end){
             swap(s[start],s[end]);
             start += 1;
             end -= 1;   
